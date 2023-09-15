ddf73f83ef36566e3db775625cb810168094e98d
Gwyneth <40921526+gweenn@users.noreply.github.com>
2023-09-15 10:47:40 +0800

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Navbar from 'react-bootstrap/Navbar';
+import '../App.css'
+
+function AppNavbar() {
+  return (
+    <>
+    <div className='test'>House Configurator</div>
+    {/* <Navbar bg="dark" variant="dark">
+    </Navbar> */}
+    </>
+  );
+};
+
+
+
+
+export default AppNavbar;
